d6b6279f697ff2c37b89cd3164d5d7ae572cacdd
Faical EL GOUIJ <faical.el-gouij@grenoble-inp.org>
2025-06-26 23:14:42 +0200

    Disabling canonical mode and managing printable caracters as well as some of the unprintable ones

--- main_text_editor.c
+++ main_text_editor.c
@@ -3,27 +3,46 @@
 #include <unistd.h>
 #include <termios.h>
 
-void enable_raw_mode() {
-    /* To turn off the ECHO feature */
-    struct termios settings;
-    tcgetattr(STDIN_FILENO, &settings);
-    settings.c_lflag &= ~ECHO;
-    /* settings.c_lflag &= ~ICANON; */
-    tcsetattr(STDERR_FILENO, TCSAFLUSH, &settings) ; 
+
+struct termios old_settings ;
+void disable_raw_mode(){
+    tcsetattr(STDERR_FILENO, TCSAFLUSH, &old_settings); 
 }
+
+void enable_raw_mode(struct termios *settings) {
+    atexit(disable_raw_mode) ; 
+    settings->c_lflag &= ~ECHO;
+    settings->c_lflag &= ~ICANON; 
+    settings->c_lflag &= ~ISIG ;
+    settings->c_iflag &= ~IXON ;
+    tcsetattr(STDERR_FILENO, TCSAFLUSH, settings); 
+}
+
 
 int main() {
-    enable_raw_mode();
+
+    struct termios new_settings ; 
+    // Old_settings are to be preserved until exit
+    tcgetattr(STDIN_FILENO, &old_settings) ;
+    // We'll be loading the actual settings in a termios type to be modified using appropriate functions
+    tcgetattr(STDIN_FILENO, &new_settings) ;
+    enable_raw_mode(&new_settings) ;
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1){
-        if (c == 'q'){break;}
-        printf("The pressed keys: %c\n", c) ;}
-   /*  returned_value = read(STDIN_FILENO, &c, 1);
-    printf("The pressed keys: %c\n", c) ;
-    while (returned_value == 1){
-        returned_value = read(STDIN_FILENO, &c, 1);
-        printf("The pressed keys: %c\n", c) ;} */
-    
+        if (c == 'q')
+        {
+            break ;
+        }
+        /* Print the printable ascii characters */
+        __uint8_t ascii = c ; 
+        if (ascii >= 32 && ascii <= 126) {
+            printf("%d ('%c')\n", c, c) ;
+        }
+        else {
+            printf("Unprintable character : %d\n", c) ;
+        }
+        //printf("The pressed keys: %c\n", c) ;
+        }
 
     return EXIT_SUCCESS ;
-}
+}:
