f3af18f6744ce1c44576f0c6e2e9176ca0386672
Faical EL GOUIJ <faical.el-gouij@grenoble-inp.org>
2025-07-02 20:39:06 +0200

    replacing all the write() commandes by a single one using a buffer, clearing the screen line by line instead of an all at once approach and creating a .h file

--- main_text_editor.c
+++ main_text_editor.c
@@ -1,44 +1,42 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <termios.h>
-#include <errno.h>
-#include <sys/ioctl.h>
-#include <ctype.h>
-#include <string.h>  // Pour strerror
-
-/* We'll use it as a macro and not as function 
-because the switch case statement (in key_process function) 
-does not support a variable returned from another function */
-#define CTRL_KEY(k) (k & 0x1f)
-
-
-
-typedef struct {
-    struct termios old_settings ;
-    struct winsize window_size; 
-} terminal_configurations ;
-
+#include "main_text_editor.h"
 terminal_configurations old_config ;
-
-
 
 ////////////////////////////////////////////////////////////////
 
-void draw_tildes(int ws_row) {   
-    for (__uint8_t i = 0; i < ws_row - 1 ; i++) {
-        write(STDOUT_FILENO, "~\r\n", 3) ;
-    }
-    write(STDOUT_FILENO, "~", 1) ;
-    write(STDOUT_FILENO, "\x1b[1;1H", 6) ;
+void draw_tildes(int ws_row,text_buffer* tildes_buff) { 
+    for (int i = 0; i < ws_row - 1; i++) {
+         /* write(STDOUT_FILENO, "~\r\n", 3) ; */
+        append_buffer(tildes_buff, "~", 1);
+        append_buffer(tildes_buff,"\x1b[K", 3); // K command : erease line; 1: left of the cursor (so we don't delete ~)
+        append_buffer(tildes_buff,"\r\n", 2);}
+    append_buffer(tildes_buff, "~", 1);
+    append_buffer(tildes_buff, "\x1b[1;1H", 6);
+    //once we're done drawing the tildes we recover the cursor;
+    append_buffer(tildes_buff,"\x1b[?25h", 6);
+    write(STDOUT_FILENO, tildes_buff->text, tildes_buff->length);
+       
+    
+   /*  write(STDOUT_FILENO, "~", 1) ;
+    write(STDOUT_FILENO, "\x1b[1;1H", 6) ; */
 }
 
 
 void clear_screen(int ws_row){
-    write(STDOUT_FILENO, "\x1b[2J", 4) ;
+    text_buffer initializing_screen = {NULL,0};
+    // we'll hide the cursor before drawing the tildes
+    append_buffer(&initializing_screen,"\x1b[?25l", 6);//?25: hide cursor; l: lowercase "L" -> disable
+
+    //we'll replace this clearing command with a line by line one in draw_tildes
+    /* append_buffer(&initializing_screen,"\x1b[2J", 4); */// J command: clear the screen ; 2: all of the screen
+    append_buffer(&initializing_screen,"\x1b[1;1H", 6); // H command : cursor position
+    /* write(STDOUT_FILENO, "\x1b[2J", 4) ;
     write(STDOUT_FILENO, "\x1b[1;1H", 6) ; // The values by default are 1;1 so we could've simply written \x1b[ whic
-                                           // would only take 3 bytes
-    draw_tildes(ws_row);
+                                           // would only take 3 bytes */
+    draw_tildes(ws_row, &initializing_screen);
+
+    
+
+    free_text_buffer(&initializing_screen);
 
 }
 
@@ -162,6 +160,30 @@ void enable_raw_mode(struct termios *settings) {
 
 ///////////////////////////////////////////////////////
 
+void append_buffer(text_buffer* current_text_buff, char* c, int length_c){
+    int length_b = current_text_buff->length;
+    char* new_text = realloc(current_text_buff->text, length_b + length_c);
+
+    // let's avoid any memory problems
+    if (new_text == NULL) {
+        printf("The text you wish to mqnipulate is NULL");
+        return;
+    }
+
+    else{
+        // now we add c to our current_text
+        for (__uint8_t i = 0; i < length_c; i++) {
+            new_text[length_b + i] = c[i];
+        }
+        current_text_buff->length += length_c;
+        current_text_buff->text = new_text;
+    }
+
+}
+
+void free_text_buffer(text_buffer* current_text_buffer){
+    free(current_text_buffer->text); // length is just an int, only text has to be freed
+}
 /////////////////////////////////////////////////////////////////////
 
 int main() {
---
+++ main_text_editor.h
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <ctype.h>
+#include <string.h>  // for strerror
+
+/* We'll use it as a macro and not as function 
+because the switch case statement (in key_process function) 
+does not support a variable returned from another function */
+#define CTRL_KEY(k) (k & 0x1f)
+
+typedef struct {
+    struct termios old_settings ;
+    struct winsize window_size; 
+} terminal_configurations ;
+
+typedef struct {
+    char* text;
+    int length;
+} text_buffer ;
+
+
+
+void draw_tildes(int ws_row, text_buffer* tildes_buff);
+
+void clear_screen(int ws_row);
+
+void kill(char* error_message);
+
+void kill(char* error_message);
+
+char read_one_key();
+
+void key_process();
+
+struct winsize cursor_position();
+
+struct winsize get_window_size();
+
+void disable_raw_mode();
+
+void enable_raw_mode(struct termios *settings);
+
+void append_buffer(text_buffer* current_text_buff, char* c, int length_c);
+
+void free_text_buffer(text_buffer* current_text_buffer);
